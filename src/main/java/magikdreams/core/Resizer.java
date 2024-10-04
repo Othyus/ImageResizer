307dac4a677133a24a34619d1a6fbbee5e809067
othyus <sebastien.remiens@gmail.com>
2024-10-05 01:04:45 +0200

    Add output pattern and multiplicator

--- src/main/java/magikdreams/core/Resizer.java
+++ src/main/java/magikdreams/core/Resizer.java
@@ -13,41 +13,52 @@ public class Resizer {
     public static final String DOWNLOADS_DIRECTORY = System.getProperty("user.home") + FileSystems.getDefault().getSeparator() + "Downloads";
     private MainPanel mainPanel;
     private File imagesDirectory;
+    private File targetImagesDirectory;
 
     public void setImagesDirectory(final File imagesDirectory) {
         this.imagesDirectory = imagesDirectory;
     }
 
-    public boolean hasSelectedFile() {
-        return imagesDirectory != null;
+    public void setTargetImagesDirectory(final File imagesDirectory) {
+        this.targetImagesDirectory = imagesDirectory;
     }
 
-    public void process(final boolean renameFiles) {
+    public boolean hasSelectedFile() {
+        return imagesDirectory != null && targetImagesDirectory != null;
+    }
+
+    public void process(final String outputPattern, final int percentage) {
         System.out.println("Processing with " + imagesDirectory);
+
+        if (!targetImagesDirectory.isDirectory()) {
+            mainPanel.popup("La destination doit être un répertoire");
+            return;
+        }
+
         if (imagesDirectory.isDirectory()) {
             boolean withError = false;
             final File[] files = imagesDirectory.listFiles();
-            if(files == null){
+            if (files == null) {
                 mainPanel.popup("Aucun fichier trouvé");
                 return;
             }
-            for(int i = 0; i < files.length; i++){
-                if(!processFile(files[i], renameFiles, i)){
+            for (int i = 0; i < files.length; i++) {
+                if (!processFile(files[i], targetImagesDirectory.getAbsolutePath(), outputPattern, percentage, i)) {
                     withError = true;
                 }
             }
-            mainPanel.popup("Terminé"+(withError ? " avec des erreurs" : ""));
+            mainPanel.popup("Terminé" + (withError ? " avec des erreurs" : ""));
         } else {
-            if(processFile(imagesDirectory, renameFiles, 1)){
+            if (processFile(imagesDirectory, targetImagesDirectory.getAbsolutePath(), outputPattern, percentage, 1)) {
                 mainPanel.popup("Terminé");
-            }else{
+            } else {
                 mainPanel.popup("Erreur");
             }
         }
     }
 
-    public boolean processFile(final File file, final boolean renameFile, final int count) {
-        System.out.println("Process file <"+file.getName()+">");
+    public boolean processFile(final File file, final String outputDirectory, final String outputPattern, final int percentage, final int count) {
+        System.out.println("Process file <" + file.getName() + ">");
         try {
             final BufferedImage sourceImage = ImageIO.read(file);
             if (sourceImage == null) {
@@ -55,11 +66,10 @@ public class Resizer {
                 return false;
             }
 
-            final String outputDirectory = file.getAbsolutePath().replaceAll(file.getName(), "");
             final String[] fileData = splitFileNameAndExtension(file.getName());
-            final BufferedImage resizedImage = resizeImage(sourceImage, fileData[1]);
+            final BufferedImage resizedImage = resizeImage(sourceImage, percentage, fileData[1]);
 
-            final File outputFile = new File(outputDirectory + FileSystems.getDefault().getSeparator() + (renameFile ? count+"."+fileData[1] : fileData[0]+"-resized."+fileData[1]));
+            final File outputFile = new File(outputDirectory + FileSystems.getDefault().getSeparator() + outputPattern.replaceAll("\\$i", "" + count) + "." + fileData[1]);
             final boolean created = outputFile.createNewFile();
             if (!created) {
                 mainPanel.popup("Impossible de créer le fichier");
@@ -73,12 +83,12 @@ public class Resizer {
         return false;
     }
 
-    public BufferedImage resizeImage(final BufferedImage image, final String format) {
-        final int newWidth = image.getWidth() / 2;
-        final int newHeight = image.getHeight() / 2;
+    public BufferedImage resizeImage(final BufferedImage image, final int percentage, final String format) {
+        final int newWidth = (image.getWidth() * percentage) / 100;
+        final int newHeight = (image.getHeight() * percentage) / 100;
         final Image scaledInstance = image.getScaledInstance(newWidth, newHeight, 1);
 
-        final BufferedImage result = new BufferedImage(newWidth, newHeight, "png".equalsIgnoreCase(format) ?  BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB);
+        final BufferedImage result = new BufferedImage(newWidth, newHeight, "png".equalsIgnoreCase(format) ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB);
 
         final Graphics2D graphics = result.createGraphics();
         graphics.drawImage(scaledInstance, 0, 0, null);
@@ -93,9 +103,9 @@ public class Resizer {
 
     private String[] splitFileNameAndExtension(final String fileName) {
         if (fileName.indexOf(".") > 0) {
-            return new String[] { fileName.substring(0, fileName.lastIndexOf(".")), fileName.substring(fileName.lastIndexOf(".")+1) } ;
+            return new String[]{fileName.substring(0, fileName.lastIndexOf(".")), fileName.substring(fileName.lastIndexOf(".") + 1)};
         } else {
-            return new String[] { fileName, fileName };
+            return new String[]{fileName, fileName};
         }
     }
 }
--- src/main/java/magikdreams/ui/MainPanel.java
+++ src/main/java/magikdreams/ui/MainPanel.java
@@ -12,12 +12,14 @@ import java.io.File;
 import static magikdreams.core.Resizer.DOWNLOADS_DIRECTORY;
 
 public class MainPanel extends JFrame {
-    private static final int WIDTH = 350;
-    private static final int HEIGHT = 175;
+    private static final int WIDTH = 380;
+    private static final int HEIGHT = 360;
 
     private final Resizer resizer;
     private final JTextField directoryPathTextField;
-    private final JCheckBox renameCheckBox;
+    private final JTextField targetDirectoryPathTextField;
+    private final JTextField renamePatternTextField;
+    private final JList<String> factorList;
 
     public MainPanel(final Resizer resizer) {
         super("Image Resizer");
@@ -37,7 +39,7 @@ public class MainPanel extends JFrame {
         final JTextPane directoryPathLabel = new JTextPane();
         directoryPathLabel.setSize(200, 25);
         directoryPathLabel.setLocation(10, 10);
-        directoryPathLabel.setText("Répertoire d'images");
+        directoryPathLabel.setText("Images à redimensionner");
         directoryPathLabel.setBackground(Color.WHITE);
         directoryPathLabel.setEditable(false);
         directoryPathLabel.setVisible(true);
@@ -45,7 +47,7 @@ public class MainPanel extends JFrame {
 
         directoryPathTextField = new JTextField();
         directoryPathTextField.setSize(195, 25);
-        directoryPathTextField.setLocation(13, 40);
+        directoryPathTextField.setLocation(13, 37);
         directoryPathTextField.setText("/");
         directoryPathTextField.setEditable(false);
         directoryPathTextField.setVisible(true);
@@ -53,30 +55,86 @@ public class MainPanel extends JFrame {
 
         final JButton directorySelectorButton = new JButton("Parcourir");
         directorySelectorButton.setSize(100, 25);
-        directorySelectorButton.setLocation(215, 40);
+        directorySelectorButton.setLocation(215, 37);
         directorySelectorButton.addActionListener(event -> openDirectorySelector());
         directorySelectorButton.setVisible(true);
         add(directorySelectorButton);
 
-        renameCheckBox = new JCheckBox();
-        renameCheckBox.setSize(20, 20);
-        renameCheckBox.setLocation(10, 70);
-        renameCheckBox.setBackground(Color.WHITE);
-        renameCheckBox.setVisible(true);
-        add(renameCheckBox);
+        final JTextPane targetDirectoryPathLabel = new JTextPane();
+        targetDirectoryPathLabel.setSize(200, 25);
+        targetDirectoryPathLabel.setLocation(10, 70);
+        targetDirectoryPathLabel.setText("Répertoire de destination");
+        targetDirectoryPathLabel.setBackground(Color.WHITE);
+        targetDirectoryPathLabel.setEditable(false);
+        targetDirectoryPathLabel.setVisible(true);
+        add(targetDirectoryPathLabel);
 
-        final JTextPane renameLabel = new JTextPane();
-        renameLabel.setSize(200, 25);
-        renameLabel.setLocation(30, 70);
-        renameLabel.setText("Renommer les fichiers ?");
-        renameLabel.setBackground(Color.WHITE);
-        renameLabel.setEditable(false);
-        renameLabel.setVisible(true);
-        add(renameLabel);
+        targetDirectoryPathTextField = new JTextField();
+        targetDirectoryPathTextField.setSize(195, 25);
+        targetDirectoryPathTextField.setLocation(13, 97);
+        targetDirectoryPathTextField.setText("/");
+        targetDirectoryPathTextField.setEditable(false);
+        targetDirectoryPathTextField.setVisible(true);
+        add(targetDirectoryPathTextField);
+
+        final JButton targetDirectorySelectorButton = new JButton("Parcourir");
+        targetDirectorySelectorButton.setSize(100, 25);
+        targetDirectorySelectorButton.setLocation(215, 97);
+        targetDirectorySelectorButton.addActionListener(event -> openTargetDirectorySelector());
+        targetDirectorySelectorButton.setVisible(true);
+        add(targetDirectorySelectorButton);
+
+        final JTextPane renamePatternLabel = new JTextPane();
+        renamePatternLabel.setSize(250, 25);
+        renamePatternLabel.setLocation(10, 130);
+        renamePatternLabel.setText("Format du fichier créé");
+        renamePatternLabel.setBackground(Color.WHITE);
+        renamePatternLabel.setEditable(false);
+        renamePatternLabel.setVisible(true);
+        add(renamePatternLabel);
+
+        renamePatternTextField = new JTextField();
+        renamePatternTextField.setSize(100, 25);
+        renamePatternTextField.setLocation(13, 157);
+        renamePatternTextField.setText("$i");
+        renamePatternTextField.setEditable(true);
+        renamePatternTextField.setVisible(true);
+        add(renamePatternTextField);
+
+        final JTextPane renamePatternExampleLabel = new JTextPane();
+        renamePatternExampleLabel.setSize(240, 25);
+        renamePatternExampleLabel.setLocation(120, 157);
+        renamePatternExampleLabel.setText("Maison-$i -> Maison-1.jpg, Maison-2.jpg");
+        renamePatternExampleLabel.setBackground(Color.WHITE);
+        renamePatternExampleLabel.setEditable(false);
+        renamePatternExampleLabel.setVisible(true);
+        add(renamePatternExampleLabel);
+
+        final JTextPane factorLabel = new JTextPane();
+        factorLabel.setSize(130, 25);
+        factorLabel.setLocation(10, 190);
+        factorLabel.setText("Multiplicateur de taille");
+        factorLabel.setBackground(Color.WHITE);
+        factorLabel.setEditable(false);
+        factorLabel.setVisible(true);
+        add(factorLabel);
+
+        final String[] factorOptions = {"10", "20", "30", "40", "50", "60", "70", "80", "90", "100", "150", "200"};
+        factorList = new JList<>(factorOptions);
+        factorList.setSelectedIndex(4);
+        factorList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        factorList.setSize(300, 25);
+        factorList.setVisibleRowCount(1);
+
+        final JPanel factorPanel = new JPanel();
+        factorPanel.setSize(300, 25);
+        factorPanel.setLocation(10, 210);
+        factorPanel.add(factorList);
+        add(factorPanel);
 
         final JButton startButton = new JButton("Start");
         startButton.setSize(100, 25);
-        startButton.setLocation(110, 100);
+        startButton.setLocation(130, 260);
         startButton.addActionListener(event -> start());
         startButton.setVisible(true);
         add(startButton);
@@ -94,12 +152,30 @@ public class MainPanel extends JFrame {
         }
     }
 
+    private void openTargetDirectorySelector() {
+        final JFileChooser fileChooser = new JFileChooser(); // *** number 1
+        fileChooser.setCurrentDirectory(new java.io.File(DOWNLOADS_DIRECTORY));
+        fileChooser.setSelectedFile(new File(""));
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        if (fileChooser.showOpenDialog(this) == JFileChooser.OPEN_DIALOG) {
+            final File selectedFile = fileChooser.getSelectedFile();
+            targetDirectoryPathTextField.setText(selectedFile.getAbsolutePath());
+            resizer.setTargetImagesDirectory(selectedFile);
+        }
+    }
+
     private void start() {
         if (!resizer.hasSelectedFile()) {
             popup("Il faut d'abord choisir un fichier ou un dossier");
             return;
         }
-        resizer.process(renameCheckBox.isSelected());
+
+        try{
+            final int percentage = Integer.parseInt(factorList.getSelectedValue());
+            resizer.process(renamePatternTextField.getText(), percentage);
+        } catch (final NumberFormatException exception) {
+            popup("Le multiplicateur choisi n'est pas un entier");
+        }
     }
 
     public void popup(final String text) {
